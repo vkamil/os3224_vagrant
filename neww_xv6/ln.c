2e73eaeaadf8295a90f103fce3d6f4ca9a7c84a8
vkamil1 <vtk207@nyu.edu>
2019-04-14 22:35:12 -0400

    m

---
+++ neww_xv6/ln.c
@@ -0,0 +1,33 @@
+<<<<<<< HEAD
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 3){
+    printf(2, "Usage: ln old new\n");
+    exit();
+  }
+  if(link(argv[1], argv[2]) < 0)
+    printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+  exit();
+}
+=======
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  if(argc != 3){
+    printf(2, "Usage: ln old new\n");
+    exit();
+  }
+  if(link(argv[1], argv[2]) < 0)
+    printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+  exit();
+}
+>>>>>>> 824ddf76d4aa59c28e182672ac8d27f225f53562
